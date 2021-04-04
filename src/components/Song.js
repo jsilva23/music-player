6faf822f0ce32708eba948e11a0c9e1998aa26d2
José da Silva <josesilva23p@gmai..com>
2021-04-04 10:34:19 +0100

    adding style to components

---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>picture</h1>
+      <h2>song name</h2>
+      <h3>Artist</h3>
+    </div>
+  );
+};
+
+export default Song;
