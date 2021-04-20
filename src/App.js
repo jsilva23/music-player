91e0f1415954a2abb9508465b6734cba009612ba
José da Silva <josesilva23p@gmai..com>
2021-04-20 12:36:15 +0100

    optimize input

--- src/App.js
+++ src/App.js
@@ -12,12 +12,6 @@ import Nav from "./components/Nav";
 import data from "./data";
 
 function App() {
-  function timeUpdateHandler(e) {
-    const current = e.target.currentTime;
-    const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
-  }
-
   //Ref
   const audioRef = useRef(null);
 
@@ -28,8 +22,27 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
+
+  function timeUpdateHandler(e) {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+
+    //calculate percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    console.log(animation);
+
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
+  }
 
   return (
     <div className="App">
